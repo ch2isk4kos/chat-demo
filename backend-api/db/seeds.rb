b0ea3e4e820e1fd632d7a49e6b8c27a80dd8e895
ch2isk4kos <ch2isk4kos@gmail.com>
2020-01-05 13:28:05 -0500

    added data to seed file

--- backend-api/db/seeds.rb
+++ backend-api/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.delete_all
+Comment.delete_all
+
+Message.create([
+    {content: "This is a message from the console", creator: "User1"},
+    {content: "This is another message", creator: "User2"},
+    {content: "And another message", creator: "User3"},
+    {content: "You guessed it, yet another message", creator: "User4"},
+    {content: "MESSAGE!", creator: "User5"}
+])
+
+Comment.create([
+    {content: "This is a comment from the console", creator: "User6", message_id: 1},
+    {content: "This is another comment from the console", creator: "User7", message_id: 2},
+])
